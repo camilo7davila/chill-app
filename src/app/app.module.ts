f2d1bd06b3ac36b7d459450f500b107b66ac6855
Wilmer Davila <javier21davila@hotmail.com>
2020-11-03 20:13:15 -0500

    configurando firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.fire),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fire:{
+    apiKey: "AIzaSyAhk2Oy0UtdxT63bH2V3GA_IA-WaOPmSqg",
+    authDomain: "chill-da5bf.firebaseapp.com",
+    databaseURL: "https://chill-da5bf.firebaseio.com",
+    projectId: "chill-da5bf",
+    storageBucket: "chill-da5bf.appspot.com",
+    messagingSenderId: "399733283876",
+    appId: "1:399733283876:web:b3cf229e82f97bf29d0cb1",
+    measurementId: "G-5PE5TCP9RT"
+  }
 };
 
 /*
